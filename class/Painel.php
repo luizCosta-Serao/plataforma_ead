5baf03f6c2e1fa612307e4e5ea56e4d6f0f4d644
Luiz Serão <luiz.serao@hotmail.com>
2024-07-21 08:37:56 -0300

    cadastro de novos alunos

--- class/Painel.php
+++ class/Painel.php
@@ -5,6 +5,16 @@
     public static function isLogin() {
       return isset($_SESSION['login']) ? true : false;
     }
+    
+    // Mensagem indicativa de sucesso ou erro
+    public static function alert($type, $message) {
+      if ($type === 'sucesso') {
+        echo '<p class="sucesso">'.$message.'</p>';
+      } else if ($type === 'erro') {
+        echo '<p class="erro">'.$message.'</p>';
+      }
+    }
   }
+
 
 ?>
