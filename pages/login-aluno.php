18050050c8b8e8085b5132f963648b2034213c41
Luiz Serão <luiz.serao@hotmail.com>
2024-07-21 15:43:40 -0300

    front-end da página de login e header básico

---
+++ pages/login-aluno.php
@@ -0,0 +1,9 @@
+<section class="login">
+  <label for="email">Email</label>
+  <input type="text" name="email" id="email">
+
+  <label for="password">Senha</label>
+  <input type="text" name="password" id="password">
+
+  <input type="submit" name="login" value="Login">
+</section>
